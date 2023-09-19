acc967d060bf892f39bea6dcfed4b4866562cc8f
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-19 16:12:43 +0100

    Finish errorElement function and css
    Finish functoin to create the error message element taking the element
    to be added to and what error message to use.

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 // Check valid
 const checkFirstName = (e) => {
-    
+    errorElement(e, "one name only")
+    // TODO - Check if there is only one word
+    // TODO - Check if there is currently an error message to avoid doubling up
 }
 
 // Check valid
 const checkLastName = (e) => {
-
+    
+    // TODO - Check if there is only one word
+    // TODO - Check if there is currently an error message to avoid doubling up
 }
 
 // Check valid
 const checkEmail = (e) => {
-
+    
+    // TODO - Check if there is only one word
 }
 
 // Check valid
-const checkPassword = (elA, elB) => {
+const checkPassword = (e) => {
     
+    // TODO - Check if there is only one word
 }
 
 //Create error text element
-const errorElement = (text) => {
+const errorElement = (e, text) => {
+    let errorMsg = document.createElement("p")
+    errorMsg.classList.add("error")
+    errorMsg.textContent = `* ${text}`
+    e.parentElement.appendChild(errorMsg)
 } 
 
 //Toggle submit button if there are any errors
@@ -30,6 +40,13 @@ const toggleBtn = e => {
 const inputs = document.querySelectorAll("input")
 inputs.forEach((input) => {
     input.addEventListener("input", e => {
-
+        switch(e.target.name){
+            case ("firstName"): checkFirstName(e.target); break
+            case ("lastName"): checkLastName(e.target); break
+            case ("email"): checkLastName(e.target); break
+            case ("checkPassword"): checkLastName(e.target); break
+            case ("lastName"): checkLastName(e.target); break
+            case ("lastName"): checkLastName(e.target); break
+        }
     })
 })
