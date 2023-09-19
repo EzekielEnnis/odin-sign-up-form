a1f4143c9839571ade2c9236257985c49848fec6
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-19 21:47:54 +0100

    Finish name validation
    Added regular expression for checking for non letter characters, if there
    are any then function adds errorElement. If no then remove errorElement.
    Also add function to remove error message to recude redundancy. Add
    margin to inputs and remove when error message is made so page doen't
    move

--- main.js
+++ main.js
@@ -1,53 +1,67 @@
 // Check valid
-const checkFirstName = (e) => {
-    
-    // TODO - Check if there is only one word
-    // TODO - Check if there is currently an error message to avoid doubling up
-}
+const checkName = (e) => {
+  const invalidCharacters = /^[a-zA-Z]*$/g;
 
-// Check valid
-const checkLastName = (e) => {
-    
-    // TODO - Check if there is only one word
-    // TODO - Check if there is currently an error message to avoid doubling up
-}
+  if (!e.value.match(invalidCharacters)) {
+    errorElement(
+      e,
+      "field can't contain numbers or special characters(!, @, #, $)"
+    );
+  } else checkRemoveElement(e)
+};
 
 // Check valid
 const checkEmail = (e) => {
-    
-    // TODO - Check if there is only one word
-}
+  // TODO - Check if there is only one word
+};
 
 // Check valid
 const checkPassword = (e) => {
-    
-    // TODO - Check if there is only one word
-}
+  // TODO - Check if there is only one word
+};
+
+const checkRemoveElement = (e) => {
+  if (e.parentElement.lastChild.className == "error") {
+    e.parentElement.lastChild.remove();
+    e.style.cssText = "margin-bottom: 1.2rem;";
+  }
+};
 
 //Create error text element
 const errorElement = (e, text) => {
-    if (e.parentElement.lastChild.className == "error") e.parentElement.lastChild.remove()
-    let errorMsg = document.createElement("p")
-    errorMsg.classList.add("error")
-    errorMsg.textContent = `* ${text}`
-    e.parentElement.appendChild(errorMsg)
-} 
+  checkRemoveElement(e);
+  let errorMsg = document.createElement("p");
+  errorMsg.classList.add("error");
+  errorMsg.textContent = `* ${text}`;
+  e.parentElement.appendChild(errorMsg);
+  e.style.cssText = "margin-bottom: 0;";
+};
 
 //Toggle submit button if there are any errors
-const toggleBtn = e => {
+const toggleBtn = (e) => {};
 
-}
-
-const inputs = document.querySelectorAll("input")
+const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
-    input.addEventListener("input", e => {
-        switch(e.target.name){
-            case ("firstName"): checkFirstName(e.target); break
-            case ("lastName"): checkLastName(e.target); break
-            case ("email"): checkLastName(e.target); break
-            case ("checkPassword"): checkLastName(e.target); break
-            case ("lastName"): checkLastName(e.target); break
-            case ("lastName"): checkLastName(e.target); break
-        }
-    })
-})
+  input.addEventListener("input", (e) => {
+    switch (e.target.name) {
+      case "firstName":
+        checkName(e.target);
+        break;
+      case "lastName":
+        checkName(e.target);
+        break;
+      case "email":
+        checkLastName(e.target);
+        break;
+      case "checkPassword":
+        checkLastName(e.target);
+        break;
+      case "lastName":
+        checkLastName(e.target);
+        break;
+      case "lastName":
+        checkLastName(e.target);
+        break;
+    }
+  });
+});
