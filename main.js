42c5ea3d7262a60efa963d953e1642b8732540c5
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-19 15:55:03 +0100

    Add basic structure of main.js & +required inputs
    Add the starts of functoins and the event listener for the inputs
    Add required attribute to all of the input fields

---
+++ main.js
@@ -0,0 +1,35 @@
+// Check valid
+const checkFirstName = (e) => {
+    
+}
+
+// Check valid
+const checkLastName = (e) => {
+
+}
+
+// Check valid
+const checkEmail = (e) => {
+
+}
+
+// Check valid
+const checkPassword = (elA, elB) => {
+    
+}
+
+//Create error text element
+const errorElement = (text) => {
+} 
+
+//Toggle submit button if there are any errors
+const toggleBtn = e => {
+
+}
+
+const inputs = document.querySelectorAll("input")
+inputs.forEach((input) => {
+    input.addEventListener("input", e => {
+
+    })
+})
