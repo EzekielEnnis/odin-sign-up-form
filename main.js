d15c2907d525cb1d1ede7ebcc225bd14871fa8b9
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-20 13:45:33 +0100

    Add function to add red borders on error inputs
    errorBorder adds or removes a red border on the input when needed

--- main.js
+++ main.js
@@ -14,35 +14,36 @@ const checkName = (e) => {
 const checkEmail = (e) => {
   if (!e.value.match(/\@?\./)) {
     errorElement(e, "please use valid email format");
-  }
-  else checkRemoveElement(e);
+  } else checkRemoveElement(e);
 };
 
 // Check valid password
 const checkPassword = (e) => {
-  if (!e.value.match(/[A-Z]/) || !e.value.match(/[1-9]/) || !e.value.match(/[a-z]/)) {
+  if (
+    !e.value.match(/[A-Z]/) ||
+    !e.value.match(/[1-9]/) ||
+    !e.value.match(/[a-z]/)
+  ) {
     errorElement(
       e,
       "must contain at least one digit and one lower and uppercase letter"
     );
   } else checkRemoveElement(e);
-  
 };
 
 //Check valid confirm password
 const checkConfPassword = (confPass, password) => {
-  
-    if (confPass.value !== password.value){
-      errorElement(confPass, "doesn't match password")
-    }
-    else checkRemoveElement(confPass);
-  };
+  if (confPass.value !== password.value) {
+    errorElement(confPass, "doesn't match password");
+  } else checkRemoveElement(confPass);
+};
 
 const checkRemoveElement = (e) => {
   if (e.parentElement.lastChild.className == "error") {
     e.parentElement.lastChild.remove();
     e.style.cssText = "margin-bottom: 1.2rem;";
   }
+  errorBorder(e, false)
 };
 
 //Create error text element
@@ -53,11 +54,21 @@ const errorElement = (e, text) => {
   errorMsg.textContent = `* ${text}`;
   e.parentElement.appendChild(errorMsg);
   e.style.cssText = "margin-bottom: 0;";
+  errorBorder(e, true);
+};
+
+const errorBorder = (e, add) => {
+  const input = e.parentElement.querySelector("input");
+  if (add) {
+    input.style["border"] = "2px solid red";
+  } else {
+    input.style["border"] = "";
+  }
 };
 
 //Check for errors before being able to submit
 const checkSubmit = (e) => {
-  const errorMsg = document.querySelector(".error")
+  const errorMsg = document.querySelector(".error");
   if (errorMsg !== null) e.preventDefault();
 };
 
@@ -76,16 +87,16 @@ inputs.forEach((input) => {
         break;
       case "password":
         checkPassword(e.target);
-        const confPass = document.querySelector("#confirmPass")
-        checkConfPassword(confPass, e.target)
+        const confPass = document.querySelector("#confirmPass");
+        checkConfPassword(confPass, e.target);
         break;
       case "confirmPass":
-        const password = document.querySelector("#password")
+        const password = document.querySelector("#password");
         checkConfPassword(e.target, password);
         break;
     }
   });
 });
 
-const submitBtn = document.querySelector("[type=submit]")
-submitBtn.addEventListener("click", e => checkSubmit(e))
+const submitBtn = document.querySelector("[type=submit]");
+submitBtn.addEventListener("click", (e) => checkSubmit(e));
